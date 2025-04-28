7b56a3b602b799a5379ce5607121ffb1b69b6c5a
Hendryansha_Palirante <hendryanshapalirante@gmail.com>
2025-04-28 17:33:52 +0700

    Penyesuaian Register Input Error

--- app/src/main/java/com/example/busymate/utils/RegisterInputError.kt
+++ app/src/main/java/com/example/busymate/utils/RegisterInputError.kt
@@ -1,14 +1,11 @@
 package com.example.busymate.utils
 
-import androidx.compose.runtime.saveable.Saver
-
 data class RegisterInputError(
-    val email: String? = null,
-    val password: String? = null,
-    val name: String? = null
+    val emailError: String? = null,
+    val passwordError: String? = null,
+    val nameError: String? = null
 )
 
-val RegisterInputErrorSaver = Saver<RegisterInputError, List<String?>>(
-    save = { listOf(it.email, it.password, it.name) },
-    restore = { RegisterInputError(it[0], it[1], it[2]) }
-)
+fun RegisterInputError.hasError(): Boolean {
+    return emailError != null || passwordError != null || nameError != null
+}
